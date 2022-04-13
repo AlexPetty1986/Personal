e1dcf11e187275beae560905b15255b1ea170afa
Alex Petty <89933754+AlexPetty1986@users.noreply.github.com>
2022-04-13 11:27:55 -0400

    Update AreaOfOctagon.py

--- Python/AreaOfOctagon.py
+++ Python/AreaOfOctagon.py
@@ -6,7 +6,6 @@ Description: The following program finds the area of an octagon
 '''
 
 import math
-side = float(input("Please enter the length of one of the sides of the
-octagon:"))
+side = float(input("Please enter the length of one of the sides of the octagon:"))
 area =( 2 * (1 + math.sqrt(2))*(side)**2)
 print("The area of the octagon is approximately:",round(area,2))
