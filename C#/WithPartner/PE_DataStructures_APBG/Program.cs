1a33f8e760c9e22518b5532138d0c9a5ff0b5ecd
Alex Petty <89933754+AlexPetty1986@users.noreply.github.com>
2022-10-17 10:43:51 -0400

    Add files via upload

---
+++ C#/WithPartner/PE_DataStructures_APBG/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PE_DataStructures_APBG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //example
+            string[] ex1 = new string[5];
+            ex1[2] = "Shiro";
+
+            //part 1
+            List<string> pets1 = new List<string>();
+            pets1.Add("Pax");
+            pets1.Add("Aiden");
+
+            //part 2
+            Pet[] pets2 = new Pet[5];
+            pets2[0] = new Pet();
+            pets2[1] = new Pet("Lacy", 7.5);
+
+            //part 3
+            List<Pet> pets3 = new List<Pet>();
+            pets3.Add(new Pet("Pax", 80));
+            pets3.Add(new Pet());
+
+            //part 4
+            bool[,] board = new bool[4,4];
+            for(int i = 0; i < board.GetLength(0); i++)
+            {
+                for(int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = false;
+
+                    if ((i+j) % 2 == 0)
+                    {
+                        board[i, j] = true;
+                    }
+                }
+            }
+
+            //part 5
+            List<int[]> grades = new List<int[]>();
+            grades.Add(new[] { 89, 87, 85 });
+            grades.Add(new[] { 73, 75 });
+            grades.Add(new[] { 88, 90, 92, 94, 99 });
+
+            //part 6
+            List<Pet>[] familyPets = new List<Pet>[4];
+            familyPets[2] = new List<Pet>();
+            familyPets[2].Add(new Pet("Samson", 11.5));
+            familyPets[3] = new List<Pet>();
+            familyPets[3].Add(new Pet("Indy", 75));
+            familyPets[3].Add(new Pet("Odin", 93));
+
+            //part 7
+            string[,] seats = new string[3, 5];
+            seats[0, 0] = "Pax";
+            seats[1, 1] = "Lacy";
+            seats[1, 3] = "Shiro";
+            seats[2, 4] = "Pidge";
+
+            //seats[1, 9] = "Uh oh"; out of range
+            //seats[0] = new string[5]; 1d array
+        }
+    }
+}
---
+++ C#/WithPartner/PE_Graphs_APBG/Graph.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Graphs_APBG
+{
+    class Graph
+    {
+        //fields
+        private Dictionary<string, List<string>> adjacency;
+
+        //constructor
+        public Graph(Dictionary<string, List<string>> adjacency)
+        {
+            this.adjacency = adjacency;
+
+            //dawing room adjacents
+            adjacency.Add("drawing room", new List<string>());
+            adjacency["drawing room"].Add("billiards room");
+            adjacency["drawing room"].Add("game room");
+            adjacency["drawing room"].Add("library");
+
+            //liberry adjacents
+            adjacency.Add("library", new List<string>());
+            adjacency["library"].Add("drawing room");
+            adjacency["library"].Add("game room");
+            adjacency["library"].Add("exit");
+
+            //billards room adjacents
+            adjacency.Add("billiards room", new List<string>());
+            adjacency["billiards room"].Add("drawing room");
+
+            //gam room adjacents
+            adjacency.Add("game room", new List<string>());
+            adjacency["game room"].Add("drawing room");
+            adjacency["game room"].Add("library");
+        }
+
+        //returns list of adjacent rooms
+        public List<string> GetAdjacentList(string room)
+        {
+            //variables
+            List<string> result = new List<string>();
+
+            //if room is not real
+            if (!adjacency.ContainsKey(room))
+            {
+                return null;
+            }
+
+            //for each adjacent room
+            foreach(string adjRoom in adjacency[room])
+            {
+                result.Add(adjRoom);
+            }
+
+            return result;
+        }
+
+        //determines if room2 is directly connected to room 1
+        public bool IsConnected(string room1, string room2)
+        {
+            //if list contains room 2 return true
+            if (adjacency[room1].Contains(room2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
